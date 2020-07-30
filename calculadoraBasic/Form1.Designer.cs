3e735abb6b48f0831ee62e5a47ad93b8f42edd0f
Crater <33011158+CraterMaik@users.noreply.github.com>
2020-07-30 11:30:31 -0500

    Calculadora básica MyBOT v1

---
+++ calculadoraBasic/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace calculadoraBasic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnMas = new System.Windows.Forms.Button();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnDivisor = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMas
+            // 
+            this.btnMas.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMas.Location = new System.Drawing.Point(410, 38);
+            this.btnMas.Name = "btnMas";
+            this.btnMas.Size = new System.Drawing.Size(100, 71);
+            this.btnMas.TabIndex = 0;
+            this.btnMas.Text = "+";
+            this.btnMas.UseVisualStyleBackColor = true;
+            this.btnMas.Click += new System.EventHandler(this.btnMas_Click);
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(145, 53);
+            this.txtValor1.Multiline = true;
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(248, 39);
+            this.txtValor1.TabIndex = 5;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(145, 130);
+            this.txtValor2.Multiline = true;
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(248, 39);
+            this.txtValor2.TabIndex = 6;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(145, 208);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(248, 39);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "VALOR 1";
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMulti.Location = new System.Drawing.Point(410, 269);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(100, 71);
+            this.btnMulti.TabIndex = 9;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenos.Location = new System.Drawing.Point(410, 115);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(100, 71);
+            this.btnMenos.TabIndex = 9;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
+            // 
+            // btnDivisor
+            // 
+            this.btnDivisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivisor.Location = new System.Drawing.Point(410, 192);
+            this.btnDivisor.Name = "btnDivisor";
+            this.btnDivisor.Size = new System.Drawing.Size(100, 71);
+            this.btnDivisor.TabIndex = 10;
+            this.btnDivisor.Text = "/";
+            this.btnDivisor.UseVisualStyleBackColor = true;
+            this.btnDivisor.Click += new System.EventHandler(this.btnDivisor_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "VALOR 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "RESULTADO";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(145, 269);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 71);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(561, 374);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDivisor);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.btnMas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CALCULADORA BASICA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMas;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnDivisor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
---
+++ calculadoraBasic/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculadoraBasic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtResultado.Enabled = false;
+        }
+
+        private void btnMas_Click(object sender, EventArgs e)
+        {
+            Operaciones(txtValor1.Text, txtValor2.Text, 1);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
+            txtResultado.Clear();
+            txtValor1.Focus();
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            Operaciones(txtValor1.Text, txtValor2.Text, 2);
+        }
+        private void Operaciones(string valor1txt, string valor2txt, int tipo)
+        {
+            int resultadoTotal, valor1 = 0, valor2 = 0;
+
+            if (valor1txt.Trim() == "")
+            {
+                MessageBox.Show("Debes agregar los valores ha sumar.");
+
+            }
+            else
+                if (valor2txt.Trim() == "")
+                {
+                    MessageBox.Show("Debes agregar los valores ha sumar.");
+                }
+                else
+                {
+
+                    bool res = Int32.TryParse(valor1txt.Trim(), out valor1);
+                    bool res2 = Int32.TryParse(valor2txt.Trim(), out valor2);
+                    if (res == false)
+                    {
+                        MessageBox.Show("Debes agregar un numero como valor");
+                        return;
+
+                    }
+                    else
+                        if (res2 == false)
+                        {
+                            MessageBox.Show("Debes agregar un numero como valor");
+                            return;
+
+                        }
+
+                    if (tipo == 1)
+                    {
+                        resultadoTotal = Convert.ToInt32(valor1txt) + Convert.ToInt32(valor2txt);
+                        txtResultado.Text = resultadoTotal.ToString();
+                    }
+                    else if (tipo == 2)
+                    {
+                        resultadoTotal = Convert.ToInt32(valor1txt) - Convert.ToInt32(valor2txt);
+                        txtResultado.Text = resultadoTotal.ToString();
+                    }
+                    else if (tipo == 3)
+                    {
+                        resultadoTotal = Convert.ToInt32(valor1txt) / Convert.ToInt32(valor2txt);
+                        txtResultado.Text = resultadoTotal.ToString();
+                    }
+                    else if (tipo == 4)
+                    {
+                        resultadoTotal = Convert.ToInt32(valor1txt) * Convert.ToInt32(valor2txt);
+                        txtResultado.Text = resultadoTotal.ToString();
+                    }
+                   
+
+                  
+
+                }
+        }
+
+        private void btnDivisor_Click(object sender, EventArgs e)
+        {
+            Operaciones(txtValor1.Text, txtValor2.Text, 3);
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            Operaciones(txtValor1.Text, txtValor2.Text, 4);
+        }
+    }
+}
